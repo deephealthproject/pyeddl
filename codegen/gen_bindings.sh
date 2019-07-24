ff08abc4b634f28d1685166d9dec0e7fba014a4e
simleo <simleo@crs4.it>
2019-07-24 10:17:22 +0200

    update codegen notes

--- codegen/gen_bindings.sh
+++ codegen/gen_bindings.sh
@@ -3,11 +3,12 @@
 set -e
 
 # this should be used from the crs4/binder docker container
-binder=/binder/build/llvm-4.0.0/build_4.0.0.linux.fd275a214859.release/bin/binder
+binder=/binder/build/llvm-4.0.0/build_4.0.0*/bin/binder
 
 # this assumes an "include" dir containing:
-#   all *addons.h files from the src dir
-#   all eddl and Eigen includes
+#   all *addons.h from the src dir, top-level (e.g., include/net_addons.h)
+#   all eddl includes, top-level (e.g., include/net.h)
+#   all Eigen includes, in an "Eigen" dir (e.g., include/Eigen/Dense)
 rm -rf ./bindings/ && mkdir bindings/
 ${binder} \
   --root-module pyeddl \
