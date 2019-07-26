f2a8036a1669e0513768a19e864279865dbbaacc
simleo <simleo@crs4.it>
2019-07-26 14:59:36 +0200

    codegen: parametrize binder exe and include dirs

--- build_bindings.sh
+++ build_bindings.sh
@@ -4,5 +4,14 @@ set -euo pipefail
 
 # re-generate src/pyeddl.cpp
 
-docker run --rm -v "${PWD}":/pyeddl -w /pyeddl/codegen crs4/binder:2f3665b ./gen_bindings.sh
+this="${BASH_SOURCE:-$0}"
+this_dir=$(cd -P -- "$(dirname -- "${this}")" && pwd -P)
+
+pyeddl_mount=/pyeddl
+
+docker run --rm -v "${this_dir}":"${pyeddl_mount}" \
+  -e BINDER_EXE='/binder/build/llvm-4.0.0/build_4.0.0*/bin/binder' \
+  -e EDDL_INCLUDE="${pyeddl_mount}"/third_party/eddl/src \
+  -e EIGEN_INCLUDE="${pyeddl_mount}"/third_party/eddl/third_party/eigen \
+  -w "${pyeddl_mount}"/codegen crs4/binder:2f3665b ./gen_bindings.sh
 cp codegen/bindings/pyeddl.cpp src/
--- codegen/gen_bindings.sh
+++ codegen/gen_bindings.sh
@@ -2,8 +2,14 @@
 
 set -e
 
-# this should be used from the crs4/binder docker container
-binder=/binder/build/llvm-4.0.0/build_4.0.0*/bin/binder
+this="${BASH_SOURCE:-$0}"
+this_dir=$(cd -P -- "$(dirname -- "${this}")" && pwd -P)
+
+# this refers to the crs4/binder docker container
+binder=${BINDER_EXE:-/binder/build/llvm-4.0.0/build_4.0.0*/bin/binder}
+
+eddl_inc=${EDDL_INCLUDE:-"${this_dir}"/../third_party/eddl/src}
+eigen_inc=${EIGEN_INCLUDE:-"${this_dir}"/../third_party/eddl/third_party/eigen}
 
 rm -rf ./bindings/ && mkdir bindings/
 ${binder} \
@@ -14,9 +20,9 @@ ${binder} \
   --single-file \
   all_bash_includes.hpp \
   -- -std=c++11 \
-  -I${PWD}/../third_party/eddl/src \
-  -I${PWD}/../third_party/eddl/third_party/eigen \
-  -I${PWD}/../src \
+  -I"${eddl_inc}" \
+  -I"${eigen_inc}" \
+  -I"${this_dir}"/../src \
   -DNDEBUG
 
 # Fix for pybind11 ImportError
