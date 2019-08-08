77ff235c69fad4353318e5da360509c33e701f1f
simleo <simleo@crs4.it>
2019-08-08 10:01:02 +0200

    update compserv addons

--- src/compserv_addons.h
+++ src/compserv_addons.h
@@ -1,2 +1,4 @@
 #define compserv_addons(cl) \
-cl.def(pybind11::init<int, vector<int>, vector<int>>());
+cl.def(pybind11::init<int, vector<int>, vector<int>, int>(), \
+       pybind11::arg("threads"), pybind11::arg("g"), pybind11::arg("f"), \
+       pybind11::arg("lsb") = 1);
