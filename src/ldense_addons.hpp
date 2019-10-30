1c95e264e0abe8f22a8b5603a1b3c8f71f37c452
simleo <simleo@crs4.it>
2019-10-30 10:16:23 +0100

    removed unused files

--- src/ldense_addons.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <pybind11/pybind11.h>
-
-template <typename type_, typename... options>
-void ldense_addons(pybind11::class_<type_, options...> &cl) {
-    cl.def(pybind11::init<Layer*, int, bool, std::string, int>(),
-           pybind11::keep_alive<1, 2>());
-}
