9fdde8c81ade1d45a4f3bf8bb4b4c4c3ce24f642
simleo <simleo@crs4.it>
2019-12-09 10:46:46 +0100

    added examples/NN/2_CIFAR10/run_all_fast.sh

---
+++ examples/NN/2_CIFAR10/run_all_fast.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# Assumes EDDL compiled for GPU.
+
+set -euo pipefail
+[ -n "${DEBUG:-}" ] && set -x
+this="${BASH_SOURCE-$0}"
+this_dir=$(cd -P -- "$(dirname -- "${this}")" && pwd -P)
+
+names=(
+    cifar_conv
+    cifar_conv_da
+    cifar_resnet
+    cifar_resnet50_da_bn
+    cifar_resnet_da_bn
+    cifar_vgg16
+    cifar_vgg16_bn
+)
+
+for n in "${names[@]}"; do
+    echo -en "\n*** ${n} ***\n"
+    python3 "${this_dir}"/${n}.py --gpu --epochs 1
+done
