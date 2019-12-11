0ff6d03823fea6f2cebe4ee6e6bb67438b95f673
simleo <simleo@crs4.it>
2019-12-11 12:11:53 +0100

    setup.py: allow arbitrary eddl installation dir

--- setup.py
+++ setup.py
@@ -8,16 +8,26 @@ EXTRA_COMPILE_ARGS = ['-std=c++11', '-fvisibility=hidden']
 LIBRARIES = ["eddl"]
 if "EDDL_WITH_CUDA" in os.environ:
     LIBRARIES.extend(["cudart", "cublas", "curand"])
+INCLUDE_DIRS = [
+    "src",
+    pybind11.get_include(),
+    pybind11.get_include(user=True)
+]
+LIBRARY_DIRS = []
+RUNTIME_LIBRARY_DIRS = []
+EDDL_DIR = os.getenv("EDDL_DIR")
+if EDDL_DIR:
+    INCLUDE_DIRS.extend([os.path.join(EDDL_DIR, "include")])
+    LIBRARY_DIRS.extend([os.path.join(EDDL_DIR, "lib")])
+    RUNTIME_LIBRARY_DIRS.extend([os.path.join(EDDL_DIR, "lib")])
 
 
 ext = Extension(
     "pyeddl._core",
     sources=["src/_core.cpp"],
-    include_dirs=[
-        "src",
-        pybind11.get_include(),
-        pybind11.get_include(user=True)
-    ],
+    include_dirs=INCLUDE_DIRS,
+    library_dirs=LIBRARY_DIRS,
+    runtime_library_dirs=RUNTIME_LIBRARY_DIRS,
     libraries=LIBRARIES,
     extra_compile_args=EXTRA_COMPILE_ARGS,
 )
