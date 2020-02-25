b984d671763da903bb3042bd8a35dd46226f7f74
simleo <simleo@crs4.it>
2020-02-25 11:02:33 +0100

    add onnx import example

---
+++ examples/onnx/import_net_from_file.py
@@ -0,0 +1,70 @@
+# Copyright (c) 2020 CRS4
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+"""\
+Import model from file in ONNX format.
+"""
+
+import argparse
+import os
+import sys
+
+import pyeddl._core.eddl as eddl
+import pyeddl._core.eddlT as eddlT
+
+
+def main(args):
+    if not os.path.isfile(args.input):
+        raise RuntimeError("input file '%s' not found" % args.input)
+
+    eddl.download_mnist()
+
+    print("importing net from", args.input)
+    net = eddl.import_net_from_onnx_file(args.input)
+    print("output size =", len(net.lout))
+
+    eddl.build(
+        net,
+        eddl.rmsprop(0.01),
+        ["soft_cross_entropy"],
+        ["categorical_accuracy"],
+        eddl.CS_GPU([1]) if args.gpu else eddl.CS_CPU(),
+        False  # do not initialize weights to random values
+    )
+
+    net.resize(args.batch_size)  # resize manually since we don't use "fit"
+    eddl.summary(net)
+
+    x_test = eddlT.load("tsX.bin")
+    y_test = eddlT.load("tsY.bin")
+
+    eddlT.div_(x_test, 255.0)
+
+    eddl.evaluate(net, [x_test], [y_test])
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument("--batch-size", type=int, metavar="INT", default=1000)
+    parser.add_argument("--gpu", action="store_true")
+    parser.add_argument("--input", metavar="STRING",
+                        default="trained_model.onnx",
+                        help="input path of the serialized model")
+    main(parser.parse_args(sys.argv[1:]))
