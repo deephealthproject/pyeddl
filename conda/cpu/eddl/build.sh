69f73cf1818bbd581ce42abd0db01a1351a0c646
simleo <simleo@crs4.it>
2020-06-26 17:36:42 +0200

    conda cpu setup: minor tweaks

--- conda/cpu/eddl/build.sh
+++ conda/cpu/eddl/build.sh
@@ -5,6 +5,7 @@ cd build
 cmake -DBUILD_EXAMPLES=OFF \
       -DBUILD_TESTS=OFF \
       -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+      -DBUILD_TARGET=CPU \
       ${SRC_DIR}
 make -j${CPU_COUNT}
 make install
