bab775c52e751fd46370ec6186120ac619ee6d19
simleo <simleo@crs4.it>
2020-06-29 13:07:39 +0200

    conda: build for multiple python versions

--- conda/cpu/test_pyeddl.sh
+++ conda/cpu/test_pyeddl.sh
@@ -3,6 +3,9 @@
 set -eo pipefail
 
 source /opt/conda/etc/profile.d/conda.sh
-conda activate test
-conda install pytest
-pytest /tests
+for v in 3.6 3.7 3.8; do
+    conda activate test${v}
+    conda install -y pytest
+    pytest /tests
+    conda deactivate
+done
--- conda/gpu/add_pyeddl_to_repo.sh
+++ conda/gpu/add_pyeddl_to_repo.sh
@@ -4,5 +4,8 @@ set -euo pipefail
 
 pkg_dir="dhealth/linux-64"
 
-pkg=$(docker run --rm pyeddl-conda bash -c 'ls -1 /opt/conda/conda-bld/linux-64/pyeddl-gpu-* | head -n 1')
-docker run --rm pyeddl-conda bash -c "cat ${pkg}" > "${pkg_dir}/$(basename ${pkg})"
+for v in 36 37 38; do
+    pkg=$(docker run --rm pyeddl-conda bash -c "ls -1 /opt/conda/conda-bld/linux-64/pyeddl-gpu-*-py${v}* | head -n 1")
+    echo "adding ${pkg}"
+    docker run --rm pyeddl-conda bash -c "cat ${pkg}" > "${pkg_dir}/$(basename ${pkg})"
+done
