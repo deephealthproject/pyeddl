4f1560b678d6cbbd92b27faab8714928436c5414
simleo <simleo@crs4.it>
2020-11-20 09:53:16 +0100

    add nlp_sentiment_lstm example

---
+++ examples/NN/4_NLP/run_all_fast.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# Assumes EDDL compiled for GPU.
+
+set -euo pipefail
+[ -n "${DEBUG:-}" ] && set -x
+this="${BASH_SOURCE-$0}"
+this_dir=$(cd -P -- "$(dirname -- "${this}")" && pwd -P)
+
+names=(
+    nlp_sentiment_rnn
+    nlp_sentiment_lstm
+)
+
+for n in "${names[@]}"; do
+    echo -en "\n*** ${n} ***\n"
+    python3 "${this_dir}"/${n}.py --gpu --epochs 1 --small
+done
