60634fefc139ee7e450a67369817740dec40c4f0
simleo <simleo@crs4.it>
2020-11-27 09:32:55 +0100

    update build_docker.sh

--- build_docker.sh
+++ build_docker.sh
@@ -5,4 +5,4 @@ set -euo pipefail
 docker build -t eddl -f Dockerfile.eddl .
 docker build -t pyeddl .
 docker build -t pyeddl-docs -f Dockerfile.docs .
-docker build -t pyeddl-docs -f Dockerfile.docs . && rm -rf /tmp/html && docker run --rm pyeddl-docs bash -c "tar -c -C /pyeddl/docs/source/_build html" | tar -x -C /tmp
+rm -rf /tmp/html && docker run --rm pyeddl-docs bash -c "tar -c -C /pyeddl/docs/source/_build html" | tar -x -C /tmp
